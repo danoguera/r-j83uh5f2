e23c37b3accc65932e14db0385693ee1c9c7a90d
Diego <danoguera@gmail.com>
2020-05-06 01:44:54 -0500

    Word counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.handleKey = this.handleKey.bind(this);
+    this.state = { value: null }
+  }
+
   render() {
+    let length = this.state.value ? this.state.value.length : 0;
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={(e) => this.handleKey(e)}>{this.state.value}</textarea>
+        <div className="counter">{length}</div>
       </div>
     );
+  }
+
+  handleKey(e){
+    this.setState({ value: e.target.value.length })
   }
 }
 
